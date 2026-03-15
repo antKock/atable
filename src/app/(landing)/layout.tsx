39758f6b2280f7f23e68cbf94855bf1278f0ebf6
Anthony Kocken <kocken.anthony@gmail.com>
2026-03-15 16:01:47 +0100

    Revert "fix: eliminate white flash on iOS overscroll and landing gradient cutoff"
    This reverts commit 2dc5a58e792aeae9909fb9ad86c5af474006ea1b.

--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -4,7 +4,7 @@ export default function LandingLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className="flex min-h-screen flex-col bg-[#EDE8E0] bg-gradient-to-b from-background to-[#EDE8E0]">
+    <div className="flex min-h-screen flex-col bg-gradient-to-b from-background to-[#EDE8E0]">
       {children}
     </div>
   )
